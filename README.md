dae0aba8e325d6c0e45473e567cb7daf93015ae9
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2023-06-28 21:08:38 +0300

    Add change read status function and comments to js file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Library
 
-This project is not finished yet.
+This project is a library. The user can add books, remove books and change the book status from read to unread and vice versa.
 
 The project can be found here: https://ginakoul.github.io/library/
--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Access elements from the html file
+
 const submit = document.querySelector(".submit");
 const books = document.querySelector(".books");
 const add = document.querySelector(".add");
@@ -5,10 +7,17 @@ const bookInfo = document.querySelector(".book-info");
 const bookDisplay = document.querySelector(".books");
 const closeInfo = document.querySelector(".close");
 
+/* Add event listener to change the display
+ of the form when the cross icon is clicked*/
+
 closeInfo.addEventListener('click',()=>{
     bookDisplay.style.display = "grid";
     bookInfo.style.display = "none";
 });
+
+/* Add event lintener to change the display
+of the library items when ADD NEW BOOK is
+clicked */
 
 add.addEventListener('click',()=>{
     bookDisplay.style.display = "none";
@@ -18,14 +27,16 @@ add.addEventListener('click',()=>{
 submit.addEventListener('click',(event)=>{
     event.preventDefault();
     addBookToLibrary();
+    changeReadStatus();
     deleteB();
 });
 
-//Array to store the objects
+// Create array variable to store the objects
 
 let myLibrary = [];
 
-//Add books from start and push them into Library
+/* Add books from start by calling the object
+constructor and push them into myLibrary array */
 
 const book1 = new Book("Hygge","Mikke","247","yes");
 const book2 = new Book("Hygge from Home","Mikke","292","yes");
@@ -34,18 +45,28 @@ const book4 = new Book("The art of creating memories","Mikke","360","no");
 
 myLibrary.push(book1,book2,book3,book4);
 
+/* Create the html elements for each book inside
+the myLibrary array */
+
 if(myLibrary.length>0){
     for(let i=0; i<=myLibrary.length-1;i++){
         let newBook = myLibrary[i];
         createCard(newBook);
-        deleteB();
     }
 }
 
-/*Takes book info from user,calls the constructor
-and pushes the object into the array*/
+/* Add event linteners for the delete icons and
+bookmark icons of every book */
+
+changeReadStatus();
+deleteB();
+
+/* Take book info from user,call the constructor
+and push the new object into the myLibrary array*/
 
 function addBookToLibrary() {
+
+    // Set display of the book section back to grid
 
     bookDisplay.style.display = "grid";
 
@@ -54,10 +75,10 @@ function addBookToLibrary() {
     let pages = document.querySelector("#pages").value;
     let read = document.querySelector('input[name="read"]:checked').value;
 
-    //Create new book object
+    // Create new book object
     const newBook = new Book(title,author,pages,read);
 
-    //Create book card
+    // Create book card
     createCard(newBook);
 
     myLibrary.push(newBook);
@@ -66,7 +87,7 @@ function addBookToLibrary() {
 
 }
 
-//Object constructor
+// Object constructor
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -78,41 +99,68 @@ function Book(title, author, pages, read){
     };
 };
 
+/* Create all the elements needed to display the
+book information and icons */
+
 function createCard(newBook){
+
+    /* Create new div inside the div of class books
+     and add class of book and id of book title
+     which is created by calling the title of the
+     book and turning it to camel style*/
+
     const book = document.createElement("div");
-    const classArray = newBook.title.split(" ");
-    for(let i=0; i<=classArray.length-1; i++){
+    const idArray = newBook.title.split(" ");
+    for(let i=0; i<=idArray.length-1; i++){
 
         let word;
 
         if(i===0){
-            word = classArray[i].charAt(0).toLowerCase()+classArray[i].slice(1);
+            word = idArray[i].charAt(0).toLowerCase()+idArray[i].slice(1);
         }else{
-            word = classArray[i].charAt(0).toUpperCase()+classArray[i].slice(1);
+            word = idArray[i].charAt(0).toUpperCase()+idArray[i].slice(1);
         }
 
-        classArray[i]= word;
+        idArray[i]= word;
     }
 
-    const classes = classArray.join("");
+    const ids = idArray.join("");
     
-    book.setAttribute('id',classes);
+    book.setAttribute('id',ids);
     book.classList.add("book");
 
     books.appendChild(book);
 
+    /* Create new div inside the div of class book
+    to hold the icons */
+
+    const icons = document.createElement("div");
+    icons.classList.add("book-icons");
+    book.appendChild(icons);
+
+    /* Create 4 new divs, 2 inside the icon holder 
+    and 2 inside the book element. Add classes to them */
+
+    const readStatus = document.createElement("div");
     const deleteBook = document.createElement("div");
     const info = document.createElement("div");
     const read = document.createElement("div");
 
+    readStatus.classList.add("read-status");
     deleteBook.classList.add("delete");
     info.classList.add("info");
     read.classList.add("read");
 
-    book.appendChild(deleteBook);
+    icons.appendChild(readStatus);
+    icons.appendChild(deleteBook);
     book.appendChild(info);
     book.appendChild(read);
 
+    /* Create 2 i elements in order to add their
+    icons and 4 divs to contain the book information
+    and add classes to them */
+
+    const bookmark = document.createElement("i");
     const cross = document.createElement("i");
     const bookTitle = document.createElement("div");
     const bookAuthor = document.createElement("div");
@@ -126,17 +174,21 @@ function createCard(newBook){
     bookPages.classList.add("bookPages");
     bookRead.classList.add("bookRead");
 
-    cross.setAttribute(`onclick`,`deleteB(${cross})`);
+    /* Add text content to each div containing book
+    information */
 
     bookTitle.textContent = `Title: ${newBook.title}`;
     bookAuthor.textContent = `Author: ${newBook.author}`;
     bookPages.textContent = `Pages: ${newBook.pages}`;
     if(newBook.read==="yes"){
         bookRead.textContent = `read`;
+        bookmark.classList.add("fa-solid", "fa-bookmark");
     }else if(newBook.read==="no"){
         bookRead.textContent = `not read yet`;
+        bookmark.classList.add("fa-regular", "fa-bookmark");
     }
 
+    readStatus.appendChild(bookmark);
     deleteBook.appendChild(cross);
     info.appendChild(bookTitle);
     info.appendChild(bookAuthor);
@@ -145,12 +197,17 @@ function createCard(newBook){
 
 }
 
+// Clear the form input values
+
 function clearForm(){
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#pages").value = "";
     document.querySelector('input[name="read"]:checked').value = "no";
 }
+
+/* Add event listener to each delete icon inside the
+book elements */
 
 function deleteB(){
 
@@ -159,7 +216,11 @@ function deleteB(){
     childs.forEach(child =>{
         child.addEventListener('click',function(){
 
-            let deleteId = this.parentElement.id;
+            /* Get the book id and compare it to the book
+            object title */
+
+            let deleteElement = this.parentElement;
+            let deleteId = deleteElement.parentElement.id;
 
             for(let i=0; i<=myLibrary.length-1;i++){
 
@@ -179,6 +240,10 @@ function deleteB(){
                 }
 
                 const title = book.join("");
+
+                /* If there is a match remove the object from
+                the myLibrary array and the html file */
+
                 if(deleteId===title){
 
                     myLibrary.splice(i,1);
@@ -186,6 +251,72 @@ function deleteB(){
                     const removeBook = document.querySelector(`#${deleteId}`);
 
                     removeBook.remove();
+                }
+            }
+        });
+    });
+}
+
+/* Add event listener to each bookmark icon inside the
+book elements */
+
+function changeReadStatus(){
+
+    const childs = document.querySelectorAll(".read-status");
+
+    childs.forEach(child =>{
+        child.addEventListener('click',function(){
+
+            /* Get the book id and compare it to the book
+            object title */
+
+            let readElement = this.parentElement;
+            let readId = readElement.parentElement.id;
+
+            for(let i=0; i<=myLibrary.length-1;i++){
+
+                let book = myLibrary[i].title.split(" ");
+                
+                for(let i=0; i<=book.length-1; i++){
+
+                    let word;
+
+                    if(i===0){
+                        word = book[i].charAt(0).toLowerCase()+book[i].slice(1);
+                    }else{
+                        word = book[i].charAt(0).toUpperCase()+book[i].slice(1);
+                    }
+
+                    book[i]= word;
+                }
+
+                const title = book.join("");
+                const bookObj = myLibrary[i];
+
+                /* If there is a match change the read status
+                of the book by changing the object.read value
+                the read text content and the icon of the book */
+
+                if(readId===title){
+
+                    const readIcon = document.querySelector(`#${readId} .fa-bookmark`);
+                    const readText = document.querySelector(`#${readId} .bookRead`);
+            
+                    if(bookObj.read==="no"){
+
+                        bookObj.read = "yes";
+                        readText.textContent = `read`;
+                        readIcon.classList.remove("fa-regular");
+                        readIcon.classList.add("fa-solid");
+        
+                    }else if(bookObj.read==="yes"){
+        
+                        bookObj.read = "no";
+                        readText.textContent = `not read yet`;
+                        readIcon.classList.remove("fa-solid");
+                        readIcon.classList.add("fa-regular");
+    
+                    }
                 }
             }
         });
